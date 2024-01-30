547accee0b1e990d5fcb268e2e7d029090b6fa37
Khalid <khalidnujaidi@gmail.com>
2024-01-30 15:03:39 +0300

    works on local network

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from website import create_app
-
+import argparse
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    parser = argparse.ArgumentParser(description="Grad Project")
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
+    app.run(host="0.0.0.0", port=args.port, debug=True)
