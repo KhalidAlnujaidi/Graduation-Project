f1a13a59ea32cbc5f1410b24ca03d53a6a84980d
Khalid <khalidnujaidi@gmail.com>
2024-02-16 10:40:51 +0300

    fixed recording logic and speed, still need to format it to be playable in browser

--- website/views.py
+++ website/views.py
@@ -22,8 +22,11 @@ import time
 
 views = Blueprint('views', __name__)
 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
 # Load custom model # best is very shitty
-model = torch.hub.load('ultralytics/yolov5', 'yolov5m') 
+model = torch.hub.load('ultralytics/yolov5', 'yolov5m').to(device)
 # Desired classes
 classes = [0, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 # 21 is bear
@@ -48,6 +51,25 @@ output_folder = "media"
 os.makedirs(output_folder, exist_ok=True)
 
 name_frequency_dict = {}
+# Define the start recording function
+def start_recording(img_BGR):
+    global is_recording, record_start_time, video_writer
+    is_recording = True
+    record_start_time = time.time()
+    print("Start Recording...")
+    # Define the video writer
+    video_filename = os.path.join(output_folder, f"recording_{time.strftime('%Y%m%d_%H%M%S')}.avi")
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_writer = cv2.VideoWriter(video_filename, fourcc, 20.0, (img_BGR.shape[1], img_BGR.shape[0]))
+
+# Define the stop recording function
+def stop_recording():
+    global is_recording, video_writer
+    is_recording = False
+    print("Stop Recording.")
+    # Release the video writer
+    video_writer.release()
+
 # Generate webcam connection
 def gen_frames():
     global name_frequency_dict, is_recording, record_start_time
@@ -57,7 +79,7 @@ def gen_frames():
     # Read until video is completed
     while(cap.isOpened()):
         
-        # Capture frame-by-fram 
+        # Capture frame-by-frame 
         success, frame = cap.read()
         if success == True:
 
@@ -66,10 +88,7 @@ def gen_frames():
             img = Image.open(io.BytesIO(frame))
             results = model(img, size=640)
 
-            #print(results.pandas().xyxy[0]['name'].tolist())
             names_column = results.pandas().xyxy[0]['name']
-
-            type(names_column)
 
             name_frequency_dict = {}
 
@@ -87,13 +106,7 @@ def gen_frames():
             detected_bear = desired_class_for_recording in name_frequency_dict.keys()
 
             if detected_bear and not is_recording:
-                is_recording = True
-                record_start_time = time.time()
-                print("Start Recording...")
-                # Define the video writer
-                video_filename = os.path.join(output_folder, f"recording_{time.strftime('%Y%m%d_%H%M%S')}.avi")
-                fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                video_writer = cv2.VideoWriter(video_filename, fourcc, 20.0, (img_BGR.shape[1], img_BGR.shape[0]))
+                start_recording(img_BGR)
 
             if is_recording:
                 # Write the frame to the video file
@@ -101,16 +114,11 @@ def gen_frames():
 
             # Check if the bear is no longer detected and stop recording after a delay
             if is_recording and not detected_bear and (time.time() - record_start_time) > record_duration:
-                is_recording = False
-                print("Stop Recording.")
-                # Release the video writer
-                video_writer.release()
+                stop_recording()
 
-
-
-            #convert remove single-dimensional entries from the shape of an array
+            # Convert remove single-dimensional entries from the shape of an array
             img = np.squeeze(results.render()) #RGB
-            # read image as BGR
+            # Read image as BGR
             img_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #BGR
 
         else:
@@ -119,7 +127,6 @@ def gen_frames():
         
         # Encode BGR image to bytes so that cv2 will convert to RGB
         frame = cv2.imencode('.jpg', img_BGR)[1].tobytes()
-        # print(frame) this was issue this was printing gibrish
         
         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         
